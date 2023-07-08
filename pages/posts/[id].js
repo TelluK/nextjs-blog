8833beb03dfcbd66678a541d0e9aee0586614521
Tellervo Kettunen <tellervo.kettunen@tuni.fi>
2023-07-08 20:57:59 +0300

    render markdown file content in blog post

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -4,11 +4,14 @@
 // each post to have the path /posts/<id>,
 // where <id> is the name of the markdown file under the top-level posts directory
 
+import utilStyles from '../../styles/utils.module.css';
+import Head from 'next/head';
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import Date from '../../components/date';
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
@@ -27,11 +30,16 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
